bf4c49dcc0611a090284f2701e6f22833a70b50d
caedenwebb <131209042+caedenwebb@users.noreply.github.com>
2026-03-29 04:15:03 -0500

    Reverted create.html and forms.py to prior state

--- MavPiz/orders/forms.py
+++ MavPiz/orders/forms.py
@@ -1,41 +1,8 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from django.utils import timezone
 from .models import Order
 
-TIME_CHOICES = [
-    ('9:00 AM', '9:00 AM'), ('9:30 AM', '9:30 AM'),
-    ('10:00 AM', '10:00 AM'), ('10:30 AM', '10:30 AM'),
-    ('11:00 AM', '11:00 AM'), ('11:30 AM', '11:30 AM'),
-    ('12:00 PM', '12:00 PM'), ('12:30 PM', '12:30 PM'),
-    ('1:00 PM', '1:00 PM'), ('1:30 PM', '1:30 PM'),
-    ('2:00 PM', '2:00 PM'), ('2:30 PM', '2:30 PM'),
-    ('3:00 PM', '3:00 PM'), ('3:30 PM', '3:30 PM'),
-    ('4:00 PM', '4:00 PM'), ('4:30 PM', '4:30 PM'),
-    ('5:00 PM', '5:00 PM'), ('5:30 PM', '5:30 PM'),
-    ('6:00 PM', '6:00 PM'), ('6:30 PM', '6:30 PM'),
-    ('7:00 PM', '7:00 PM'), ('7:30 PM', '7:30 PM'),
-    ('8:00 PM', '8:00 PM'), ('8:30 PM', '8:30 PM'),
-    ('9:00 PM', '9:00 PM'), ('9:30 PM', '9:30 PM'),
-    ('10:00 PM', '10:00 PM'), ('10:30 PM', '10:30 PM'),
-]
-
-
 class OrderCreateForm(forms.ModelForm):
-    # This guarantees Django draws a dropdown menu for this specific field
-    pickupTime = forms.ChoiceField(choices=TIME_CHOICES, label="Pickup Time")
-
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
-                  'postal_code', 'city', 'pickupTime']
-
-        '''widgets = {
-            'pickup_date': forms.DateInput(attrs={'type': 'date'}),
-        }'''
-
-    '''def clean_pickup_date(self):
-        date = self.cleaned_data.get('pickup_date')
-        if date and date < timezone.now().date():
-            raise ValidationError("You cannot select a date in the past!")
-        return date'''
+                  'postal_code', 'city']
